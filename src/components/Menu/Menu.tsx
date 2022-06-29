fb7cf2b02856c74e86270f74b8a1ce7810804073
Cravcenco Nichita <nice_arti@mail.ru>
2022-06-29 22:32:33 +0300

    feat: add 3D planet with texture

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -4,6 +4,8 @@ import { Row, Col } from "antd";
 import styles from "./Menu.module.scss";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
+// import {earth} from '%PUBLIC_URL%/textures/earth.jpg';
+import * as THREE from "three";
 
 const StyledMenu = styled(Row)`
   height: 100%;
@@ -14,6 +16,40 @@ const StyledMenu = styled(Row)`
     padding-left: 100px;
   }
 `;
+
+const camera = new THREE.PerspectiveCamera(
+  70,
+  window.innerWidth / window.innerHeight,
+  0.01,
+  10
+);
+camera.position.z = 1;
+
+const scene = new THREE.Scene();
+
+const geometry = new THREE.SphereGeometry(0.4, 30, 30);
+const texture = new THREE.TextureLoader().load("textures/earth.jpg");
+const material = new THREE.MeshBasicMaterial({ map: texture });
+
+const mesh = new THREE.Mesh(geometry, material);
+mesh.rotation.x = 50;
+mesh.rotation.z = 50;
+scene.add(mesh);
+
+
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setAnimationLoop(animation);
+document.body.appendChild(renderer.domElement);
+
+// animation
+
+function animation(time: any) {
+  mesh.rotation.y = time / 15000 * -1;
+
+  // render object each frame
+  renderer.render(scene, camera);
+}
 
 export const Menu: React.FC = () => {
   return (
